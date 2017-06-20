370c923dabcdf3012d15589d748e6175192c3067
gonghaimin <2535355994@qq.com>
2017-06-20 21:47:54 +0800

    22222

---
+++ Server/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace Server
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Web API 配置和服务
+            //跨域配置,使用Nuget搜索“microsoft.aspnet.webapi.cors”，安装第一个
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            config.Filters.Add(new Filters.ApiSecurityFilter());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Server/Common/CacheFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace Server.Common
+{
+    public class CacheFilterAttribute : ActionFilterAttribute
+    {
+        //缓存
+        public int CacheTimeDuration { get; set; }
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            actionExecutedContext.Response.Headers.CacheControl = new CacheControlHeaderValue
+            {
+                MaxAge = TimeSpan.FromSeconds(CacheTimeDuration),
+                MustRevalidate = true,
+                Public = true
+            };
+        }
+    }
+}
